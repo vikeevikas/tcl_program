e3aa5e78c95cc5d223abbe7ef6723f036dcae230
Vikas Kumar <vikee.vikas@gmail.com>
2023-07-08 18:55:49 +0530

    Added SDC file creation steps

--- vsdflow.tcl
+++ vsdflow.tcl
@@ -1,5 +1,9 @@
 #! /bin/env tclsh
 
+puts "Set generate_sdc to '1' if you want to generate SDC from data given in excel sheet. If you already have sdc, save it in outdir_<DesignName> as <DesignName>.sdc and set this variable to '0'"
+set generate_sdc 1
+puts "Setting variable 'generate_sdc' to $generate_sdc\n"
+
 #-----------------------------------------------------------#
 #----- Checks whether vsdsynth usage is correct or not -----#
 #-----------------------------------------------------------#
@@ -93,5 +97,75 @@ if {! [file exists $ConstraintsFile] } {
         puts "\nInfo: Constraints file found in path $ConstraintsFile"
 }
 
+
+#----------------------------------------------------------------------------#
+#----------------------  Constraints FILE creations--------------------------#
+#----------------------------- SDC Format -----------------------------------#
+#----------------------------------------------------------------------------#
+if {$generate_sdc == 1} {
+
+puts "\nInfo: Dumping SDC constraints for $DesignName"
+::struct::matrix constraints
+set chan [open $ConstraintsFile]
+csv::read2matrix $chan constraints  , auto
+close $chan
+set number_of_rows [constraints rows]
+set number_of_columns [constraints columns]
+
+puts "Constraints File number of rows = $number_of_rows"
+puts "Constraints File number of columns = $number_of_columns"
+
+return
+
+#-----check row number for "clocks" and column number for "IO delays and slew section" in constraints.csv---##
+set clock_start [lindex [lindex [constraints search all CLOCKS] 0 ] 1]
+set clock_start_column [lindex [lindex [constraints search all CLOCKS] 0 ] 0]
+set clock_period [constraints get cell [expr {$clock_start+1}] [expr {$clock_start_column+1}]]
+
+#-----check row number for "inputs" section in constraints.csv---##
+set input_ports_start [lindex [lindex [constraints search all INPUTS] 0 ] 1]
+
+#-----check row number for "outputs" section in constraints.csv---##
+set output_ports_start [lindex [lindex [constraints search all OUTPUTS] 0 ] 1]
+
+#-------------------clock constraints--------------------##
+#-------------------clock latency constraints------------#
+
+set clock_early_rise_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  early_rise_delay] 0 ] 0]
+
+set clock_early_fall_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  early_fall_delay] 0 ] 0]
+
+set clock_late_rise_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  late_rise_delay] 0 ] 0]
+
+set clock_late_fall_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  late_fall_delay] 0 ] 0]
+
+#-------------------clock transition constraints------------#
+
+set clock_early_rise_slew_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  early_rise_slew] 0 ] 0]
+
+set clock_early_fall_slew_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  early_fall_slew] 0 ] 0]
+
+set clock_late_rise_slew_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  late_rise_slew] 0 ] 0]
+
+set clock_late_fall_slew_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}]  late_fall_slew] 0 ] 0]
+
+set sdc_file [open $OutputDirectory/$DesignName.sdc "w"]
+set i [expr {$clock_start+1}]
+set end_of_ports [expr {$input_ports_start-1}]
+puts "\nInfo-SDC: Working on clock constraints....."
+while { $i < $end_of_ports } {
+        puts -nonewline $sdc_file "\ncreate_clock -name [constraints get cell 0 $i] -period [constraints get cell 1 $i] -waveform \{0 [expr {[constraints get cell 1 $i]*[constraints get cell 2 $i]/100}]\} \[get_ports [constraints get cell 0 $i]\]"
+	puts -nonewline $sdc_file "\nset_input_transition -rise -min [constraints get cell $clock_early_rise_slew_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+	puts -nonewline $sdc_file "\nset_input_transition -fall -min [constraints get cell $clock_early_fall_slew_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_transition -rise -max [constraints get cell $clock_late_rise_slew_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_transition -fall -max [constraints get cell $clock_late_fall_slew_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_delay  -min -rise [constraints get cell $clock_early_rise_delay_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_delay  -min -fall [constraints get cell $clock_early_fall_delay_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_delay  -max -rise [constraints get cell $clock_late_rise_delay_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        puts -nonewline $sdc_file "\nset_input_delay  -max -fall [constraints get cell $clock_late_fall_delay_start $i] -clock [constraints get cell 0 $i] \[get_ports [constraints get cell 0 $i]\]"
+        set i [expr {$i+1}]
+}
+
+}
 
 return
