398b3ffd0dc08e027a0785fd2ebb6c056678ac4c
Vikas Kumar <vikee.vikas@gmail.com>
2023-07-08 17:37:53 +0530

    Added checks in vsdflow.tcl

--- vsdflow.tcl
+++ vsdflow.tcl
@@ -1,28 +1,52 @@
 #! /bin/env tclsh
 
+#-----------------------------------------------------------#
+#----- Checks whether vsdsynth usage is correct or not -----#
+#-----------------------------------------------------------#
+
+set working_dir [exec pwd]
+set vsd_array_length [llength [split [lindex $argv 0] .]]
+set input [lindex [split [lindex $argv 0] .] $vsd_array_length-1]
+
+if {![regexp {^csv} $input] || $argc!=1 } {
+	puts "Error in usage"
+	puts "Usage: ./vsdsynth <.csv>"
+	puts "where <.csv> file has below inputs"
+	exit
+} else {
 #-----------------------------------------------------------------------------------------------------------------------------------------------------#
 #------ converts .csv to matrix and creates initial variables "DesignName OutputDirectory NetlistDirectory EarlyLibraryPath LateLibraryPath"----------#
 #----------- If you are modifying this script, please use above variables as starting point. Use "puts" command to report above variables-------------#
 #-----------------------------------------------------------------------------------------------------------------------------------------------------#
-set filename [lindex $argv 0]
-package require csv
-package require struct::matrix
-struct::matrix m
-set f [open $filename]
-csv::read2matrix $f m , auto
-close $f
-set columns [m columns]
-m add columns $columns
-m link my_arr
-set num_of_rows [m rows]
-set i 0
-while {$i < $num_of_rows} {
-	puts "\nInfo: Setting $my_arr(0,$i) as '$my_arr(1,$i)'"
-	if {$i == 0} {
-		set [string map {" " ""} $my_arr(0,$i)] $my_arr(1,$i)
+	set filename [lindex $argv 0]
+	package require csv
+	package require struct::matrix
+	struct::matrix m
+	set f [open $filename]
+	csv::read2matrix $f m , auto
+	close $f
+	set columns [m columns]
+	m add columns $columns
+	m link my_arr
+	set num_of_rows [m rows]
+	set i 0
+	while {$i < $num_of_rows} {
+		 puts "\nInfo: Setting $my_arr(0,$i) as '$my_arr(1,$i)'"
+		 if {$i == 0} {
+			 set [string map {" " ""} $my_arr(0,$i)] $my_arr(1,$i)
 		 } else {
-		set [string map {" " ""} $my_arr(0,$i)] [file normalize $my_arr(1,$i)]
+			 set [string map {" " ""} $my_arr(0,$i)] [file normalize $my_arr(1,$i)]
 		 }
-		set i [expr {$i+1}]
-}
+		  set i [expr {$i+1}]
+	}
+} 
 
+puts "\nInfo: Below are the list of initial variables and their values. User can use these variables for further debug. Use 'puts <variable name>' command to query value of below variables"
+puts "DesignName = $DesignName"
+puts "OutputDirectory = $OutputDirectory"
+puts "NetlistDirectory = $NetlistDirectory"
+puts "EarlyLibraryPath = $EarlyLibraryPath"
+puts "LateLibraryPath = $LateLibraryPath"
+puts "ConstraintsFile = $ConstraintsFile"
+
+return
